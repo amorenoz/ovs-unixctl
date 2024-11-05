545513ccd4b4abcdb4897d5ce90bf2940d90c060
Adrian Moreno <amorenoz@redhat.com>
2024-11-05 22:51:10 +0100

    ovs: allow arbitrary command runner
    Test it using a vlog/set and vlog/list.
    
    Also, add an example in the module documentation.
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,21 @@
 //! OpenvSwitch application control (appctl) library.
+//!
+//! Example:
+//! ```no_run
+//! use ovs_unixctl::OvsUnixCtl;
+//!
+//! let mut unixctl = OvsUnixCtl::new().unwrap();
+//! let commands = unixctl.list_commands().unwrap();
+//! println!("Available commands");
+//! for (command, args) in commands.iter() {
+//!     println!("{command}: {args}");
+//! }
+//!
+//! let bonds = unixctl.run("bond/list", None).unwrap();
+//! println!("{}", bonds.unwrap());
+//! let bond0 = unixctl.run("bond/show", Some(&["bond0"])).unwrap();
+//! println!("{}", bond0.unwrap());
+//! ```
 
 //FIXME
 #[allow(dead_code)]
